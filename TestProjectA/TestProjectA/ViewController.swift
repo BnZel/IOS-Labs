1bf3a885042cf6aafb3340c66c9e66ead110f034
BnZel <biozxel1111@gmail.com>
2020-02-03 11:11:37 -0500

    initial commit

--- TestProjectA/TestProjectA/ViewController.swift
+++ TestProjectA/TestProjectA/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblA: UILabel!
+    @IBOutlet weak var lblB: UILabel!
+    
+    @IBOutlet weak var txtA: UITextField!
+    @IBOutlet weak var txtB: UITextField!
+    
+    @IBAction func btnAction(_ sender: Any) {
+        
+        lblA.text = txtB.text
+        lblB.text = txtA.text
+        
+        print("I'm the action button!");
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
