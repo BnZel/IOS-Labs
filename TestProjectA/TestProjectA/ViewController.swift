c97773146b684de97359c8acf1f32a454ce5a9c7
Tech <tech@Techs-iMac-60.local>
2020-02-03 08:34:42 -0500

    initial commit

---
+++ TestProjectA/TestProjectA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProjectA
+//
+//  Created by Tech on 2020-02-03.
+//  Copyright © 2020 Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
