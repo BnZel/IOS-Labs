cd1d9d0c9f2b9393e9786b2529da413a51fc1030
BnZel <40049495+BnZel@users.noreply.github.com>
2020-04-07 19:49:45 -0400

    Added March 31st and April 7th lab demos

---
+++ AlertController-starter/AlertControllerExample/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  AlertControllerExample
+//
+//  Created by Parrot on 2020-03-28.
+//  Copyright © 2020 Parrot. All rights reserved.
+//
+
+import UIKit
+
+// ALERT BOX CHECKLIST:
+// - Did you create a new UIAlertController object and save to variable? (abc)
+// - Did you set the title and message parameters?
+// - Did you choose the style of your Alert Box? (Alert vs. ActionSheet)
+// - Did you add buttons? (.addAction)
+// - Did you remember to SHOW the alert box? (self.present(abc, animated:true))
+// OPTIONAL:
+// - Do your buttons need "click handlers"?
+// - Do you need to include text boxes? (.addTextField(....))
+
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    
+    // MARK: Default Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+
+
+}
+
---
+++ PickerViewExample/PickerViewsExample/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  PickerViewsExample
+//
+//  Created by Parrot on 2020-03-27.
+//  Copyright © 2020 Parrot. All rights reserved.
+//
+
+// CHECKLIST: Did you remember to:
+// - Create an outlet for your PickerView
+// - Make a data souce?
+// - Add PickerView protocols to the class?
+// - Setup the protocols in the viewDidLoad()?
+// - Add mandatory PickerView functions?
+//      - numcomponents
+//      - pickerView(numRowsInComponent)
+//      - pickerView(titleForRow)
+// - Customize functions to use your data source?
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    // put outlets here
+    
+    // MARK: Default functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+    // put your actions here
+    
+}
+
